f178e21c88ab0f60823b8863b82a2429ca7b5328
Gigitogigs <ngigemwaniki2004@gmail.com>
2023-06-19 00:33:16 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /*
- * main multilingual puzzle
+ * main- multilingual puzzle
  *
  * return (0)
  */
 int main(void)
 {
-	puts (""Programmig is like builing a multilingual puzzle"\n");
-	return(0);
+	putsi(""Programmig is like builing a multilingual puzzle"\n");
+	return (0);
 }
