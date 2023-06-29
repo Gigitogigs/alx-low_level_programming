227c94ecc4814e75eec6eb1192d65eea1e14a38f
Gigitogigs <ngigemwaniki2004@gmail.com>
2023-06-29 06:38:23 -0700

    task 10

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - a code that prints out integers
+ *
+ * Return: Always (0) Success
+ */
+void print_number(int n)
+{
+	_putchar("Enter an integer:\n");
+	scanf(%d,&n);
+        _putchar(char n);
+}
