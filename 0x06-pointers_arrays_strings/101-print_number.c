588bfe8ff43c6cd21365242991ab9d1c248eabdf
Gigitogigs <ngigemwaniki2004@gmail.com>
2023-06-29 06:43:21 -0700

    task 10

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,7 +8,12 @@
  */
 void print_number(int n)
 {
-	_putchar("Enter an integer:\n");
-	scanf(%d,&n);
-        _putchar(char n);
+	if (n < 0)
+	{
+		putchar('-');
+		n = -n;
+	}
+	if (n / 10)
+		print_number(n / 10):
+	putchar(n % 10 + '0');
 }
