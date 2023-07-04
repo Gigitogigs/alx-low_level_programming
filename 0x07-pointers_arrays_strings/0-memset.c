23d7b4ee58db8fa19407fde4c0e50136ab9a49ab
Gigitogigs <ngigemwaniki2004@gmail.com>
2023-07-04 00:26:54 -0700

    task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - a function that fills memory with cinstant byte
+ * _memset -  function
+ * @s: variable caharcter
+ * @b: avriable charatcter
+ * @n: varaible integer
+ * Return: pointer location
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	char *s = s;
+	while (n--)
+	{ 
+		*s = b;
+		s++;
+	}
+	return (*s);
+}
---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,10 @@
+int putchar(char c);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int_strspn(char *s, char *accept);
+char *_strpbk(char *s, char *accept);
+char *_strstr(char *haystsck, char *needle);
+void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
+
