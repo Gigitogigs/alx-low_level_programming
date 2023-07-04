f66928ce3c673efdc1c2db79e1c5adf933758c17
Gigitogigs <ngigemwaniki2004@gmail.com>
2023-07-04 01:24:02 -0700

    task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strchr - function that locates character in astring
+ * @s:pointer to chatarcetr s
+ * @c:variable chararcter
+ * Return: returns occurence of characte c
+ */
+char *_strchr(char *s, char c)
+{
+	while (char = 'c')
+	{
+		return (*s);
+		char++;
+	}
+	return (NULL);
+}
+ 
