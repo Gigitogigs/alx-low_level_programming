43d97ce8b8affb6c05248cabb3bc17e49c7e62dc
Gigitogigs <ngigemwaniki2004@gmail.com>
2023-07-25 12:37:49 -0700

    try

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,11 +5,11 @@
  * @argv: The arguement values
  * Return: Always 0 (Success)
  */
-int main(int argc, char *argv)
+int main(int argc, char **argv)
 {
 	while (argc--)
 	{
-		printf("%s\n",argv++);
+		printf("%s\n", *argv++);
 	}
 
 	return (0);
